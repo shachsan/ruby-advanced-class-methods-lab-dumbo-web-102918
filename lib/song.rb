80be7c60b8bd35af8f4c8aae953b09c979856d95
SANJAY K SAI <shachsan@hotmail.com>
2018-11-01 03:06:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song|song.name==name}
   end
-
+  
   def self.find_or_create_by_name(name)
     song = self.find_by_name(name)
     if song
@@ -46,6 +46,6 @@ class Song
 
   end
 
-  def 
+  def
 
 end
